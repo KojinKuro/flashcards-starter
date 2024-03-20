1607781b613766aa8c254641ea29e1fb7113046f
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-03-20 14:58:01 -0700

    Update timer to new functions

--- src/round.js
+++ src/round.js
@@ -1,51 +1,52 @@
 const { evaluateGuess } = require("./turn");
-const { startTimer, endTimer, convertTimerToString } = require("./timer");
+const { startTimer, calculateTime, convertTimerToString } = require("./timer");
 
 function createRound(deck) {
-  return {
-    deck: deck,
-    currentCard: deck[0],
-    turns: 0,
-    incorrectGuesses: [],
-    startTimer: startTimer(),
-  };
+	return {
+		deck: deck,
+		currentCard: deck[0],
+		turns: 0,
+		incorrectGuesses: [],
+		startTimer: startTimer(Date.now()),
+	};
 }
 
 function takeTurn(guess, round) {
-  const evaluation = evaluateGuess(guess, round.currentCard.correctAnswer);
-  if (evaluation === "incorrect!")
-    round.incorrectGuesses.push(round.currentCard.id);
-  round.turns += 1;
-  round.currentCard = round.deck[round.turns];
-  return evaluation;
+	const evaluation = evaluateGuess(guess, round.currentCard.correctAnswer);
+	if (evaluation === "incorrect!")
+		round.incorrectGuesses.push(round.currentCard.id);
+	round.turns += 1;
+	round.currentCard = round.deck[round.turns];
+	return evaluation;
 }
 
 function calculatePercentCorrect(round) {
-  const percentage =
-    (round.turns - round.incorrectGuesses.length) / round.turns;
-  return Math.floor(percentage * 100);
+	const percentage =
+		(round.turns - round.incorrectGuesses.length) / round.turns;
+	return Math.floor(percentage * 100);
 }
 
 function endRound(round) {
-  const percentage = calculatePercentCorrect(round);
-  const timer = convertTimerToString(endTimer(round.startTimer));
-  const message = `** Round over! ** You answered ${percentage}% of the questions correctly! You took ${timer} to complete the game.`;
-
-  console.log(message);
-  return message;
+	const percentage = calculatePercentCorrect(round);
+	const timer = convertTimerToString(
+		calculateTime(round.startTimer, Date.now()),
+	);
+	const message = `** Round over! ** You answered ${percentage}% of the questions correctly! You took ${timer} to complete the game.`;
+	console.log(message);
+	return message;
 }
 
 function resetRound(round) {
-  round.turns = 0;
-  round.currentCard = round.deck[round.turns];
-  round.incorrectGuesses = [];
-  return round;
+	round.turns = 0;
+	round.currentCard = round.deck[round.turns];
+	round.incorrectGuesses = [];
+	return round;
 }
 
 module.exports = {
-  createRound,
-  takeTurn,
-  calculatePercentCorrect,
-  endRound,
-  resetRound,
+	createRound,
+	takeTurn,
+	calculatePercentCorrect,
+	endRound,
+	resetRound,
 };
